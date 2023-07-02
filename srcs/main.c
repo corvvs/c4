230626d5ab659bfaecd06eb65548478dc6d00fba
河田鷹介 <benefium@gmail.com>
2023-07-02 23:01:06 +0900

    simplified game_loop

--- srcs/main.c
+++ srcs/main.c
@@ -203,6 +203,37 @@ int try_play(t_game* game, int col)
 	return 1; // continue
 }
 
+int	put_hand(t_game* game) {
+	int			res = -1;
+	t_board*	board = &game->board;
+	if (game->current_player == 0)
+	{
+		print_board(board);
+		ft_putstr_fd("player's turn:\n", STDOUT_FILENO);
+		for (; res < 0;)
+		{
+			unsigned int	col;
+			if (get_col(&col)) {
+				res = try_play(game, col - 1);
+				if (res >= 0) { break; }
+			}
+			ft_putstr_fd("accepts only: 1 ~ ", STDOUT_FILENO);
+			ft_putnbr_fd(game->board.WIDTH, STDOUT_FILENO);
+			ft_putstr_fd("\n", STDOUT_FILENO);
+		}
+	}
+	else if (game->current_player == 1)
+	{
+		ft_putstr_fd("AI's turn: ", STDOUT_FILENO);
+		unsigned int	col = ai_decide(game);
+		ft_putnbr_fd(col + 1, STDOUT_FILENO), ft_putstr_fd("\n", STDOUT_FILENO);
+		if ((res = try_play(game, col)) < 0)
+			ft_putstr_fd("ERROR: AI messed up\n", 2);
+	}
+	game->game_turn += 1;
+	return res;
+}
+
 // TODO move to main.c
 // TODO implement AI
 void	game_loop(t_game* game)
@@ -210,31 +241,7 @@ void	game_loop(t_game* game)
 	t_board*	board = &game->board;
 	for (;;)
 	{
-
-		int	res = -1;
-		if (game->current_player == 0)
-		{
-			print_board(board);
-			ft_putstr_fd("player's turn:\n", STDOUT_FILENO);
-			for (; res < 0;)
-			{
-				unsigned int	col;
-				if (get_col(&col) != true) {
-					continue;
-				}
-				res = try_play(game, col - 1);
-			}
-		}
-		else if (game->current_player == 1)
-		{
-			ft_putstr_fd("AI's turn: ", STDOUT_FILENO);
-			unsigned int	col = ai_decide(game);
-			ft_putnbr_fd(col + 1, STDOUT_FILENO), ft_putstr_fd("\n", STDOUT_FILENO);
-			if ((res = try_play(game, col)) < 0)
-				ft_putstr_fd("ERROR: AI messed up\n", 2);
-		}
-		game->game_turn += 1;
-		if (res == 0)
+		if (put_hand(game) == 0)
 		{
 			print_board(board);
 			ft_putstr_fd((char*[]){"you win!\n", "AI wins!\n"}[game->current_player], STDOUT_FILENO);
